838bdb921e16285d5df622aebd79c94c776fbb8d
wang <yaquawa@gmail.com>
2017-02-18 21:43:05 +0900

    update

---
+++ Foundation/ComposerScripts.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Laraish\Foundation;
+
+use Composer\Script\Event;
+
+class ComposerScripts
+{
+    /**
+     * Handle the post-install and post-update Composer event.
+     *
+     * @param  \Composer\Script\Event $event
+     *
+     * @return void
+     */
+    public static function renameHelperFunctions(Event $event)
+    {
+        $vendorDir   = $event->getComposer()->getConfig()->get('vendor-dir');
+        $helpersPath = $vendorDir . '/laravel/framework/src/Illuminate/Foundation/helpers.php';
+
+        if ( ! file_exists($helpersPath)) {
+            return;
+        }
+
+        $content = file_get_contents($helpersPath);
+        $content = str_replace("function_exists('__')", "function_exists('___')", $content);
+        $content = str_replace('function __', 'function ___', $content);
+        file_put_contents($helpersPath, $content);
+    }
+}
